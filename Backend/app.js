89ee610ac41bfbe4795daa2ecbb0f642cfea8da7
Eveyas <140725035+Eveyas@users.noreply.github.com>
2025-07-31 13:29:55 -0500

    cambioss

--- Backend/app.js
+++ Backend/app.js
@@ -3,8 +3,8 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-// const port = process.env.SERVER_PORT; 
-const port = process.env.PORT || 3000;
+const port = process.env.SERVER_PORT; 
+// const port = process.env.PORT;
 const db = require('./Config/Db');
 
 const corsOptions = {
